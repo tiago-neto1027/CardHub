da94ad75a007eed4cb481ddd68db6599cb9e518e
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-11-25 19:55:07 +0000

    Revert "Cards, Listings & Game"
    This reverts commit 1b301088d2eee3cf599a2902b708aa972a2827e3.

--- web/backend/views/layouts/sidebar.php
+++ web/backend/views/layouts/sidebar.php
@@ -55,9 +55,9 @@
 
                     /*['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
-                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],*/
+                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
                     ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    /*['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],*/
+                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],*/
                     /*['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
                     ['label' => 'Level1'],
                     [
