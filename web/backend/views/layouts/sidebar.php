dddf776f22033066b1031cb30bad60e65b251ce9
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-11-25 22:45:59 +0000

    Sidebar changes

--- web/backend/views/layouts/sidebar.php
+++ web/backend/views/layouts/sidebar.php
@@ -1,19 +1,17 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- TODO: Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
+        <!--User-->
+        <div class="user-panel my-2 pb-2 mx-0 d-flex">
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block"><?= Yii::$app->user->identity->username ?? 'Guest' ?></a>
             </div>
         </div>
 
@@ -45,18 +43,33 @@
                             ['label' => 'Inactive Page', 'iconStyle' => 'far'],
                         ]
                     ],*/
-                    ['label' => 'Home', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                    ['label' => 'User Management', 'url' =>['user/index'], 'iconStyle' => 'far'],
-                    ['label' => 'Game Management', 'url' =>['game/index'], 'iconStyle' => 'far'],
-                    ['label' => 'Product Management', 'iconStyle' => 'far'],
-                    ['label' => 'Cards Management', 'url' =>['card/index'], 'iconStyle' => 'far'],
-                    ['label' => 'Reviews', 'iconStyle' => 'far'],
-                    ['label' => 'Statistics', 'iconStyle' => 'far'],
+                    ['label' => 'Dashboard', 'url' => ['site/index'], 'icon' => 'tachometer-alt'],
+
+
+                    ['label' => 'CRUD MENUS', 'header' => true],
+                    ['label' => 'User Management', 'url' => ['user/index'], 'icon' => 'user'],
+                    ['label' => 'Game Management', 'url' => ['game/index'], 'icon' => 'gamepad'],
+                    ['label' => 'Cards Management', 'url' => ['card/index'], 'icon' => 'dragon'],
+                    ['label' => 'Product Management', 'icon' => 'shopping-cart'],
+
+
+                    ['label' => 'OTHERS', 'header' => true],
+                    [
+                        'label' => 'TODO',
+                        'icon' => 'exclamation',
+                        'items' => [
+                            ['label' => 'Reviews', 'iconStyle' => 'far'],
+                            ['label' => 'Statistics', 'iconStyle' => 'far'],
+                            ['label' => 'Accept Cards', 'iconStyle' => 'far'],
+                            ['label' => 'Card Sales', 'iconStyle' => 'far'],
+                            ['label' => 'Product Sales', 'iconStyle' => 'far'],
+                        ]
+                    ],
 
                     /*['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],*/
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                    ['label' => 'Gii', 'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
                     /*['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],*/
                     /*['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
                     ['label' => 'Level1'],
@@ -76,7 +89,7 @@
                             ['label' => 'Level2', 'iconStyle' => 'far']
                         ]
                     ],
-                    ['label' => 'Level1'],*/
+                    ['label' => 'Level1'],/*
                     /*['label' => 'LABELS', 'header' => true],
                     ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
                     ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
