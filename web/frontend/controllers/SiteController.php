c9df78300323929bc7b9befece479616bc062c38
Psousa92 <voltaire@live.com.pt>
2024-11-29 22:56:39 +0000

    products on front page
    products on front page

--- web/frontend/controllers/SiteController.php
+++ web/frontend/controllers/SiteController.php
@@ -11,6 +11,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use common\models\LoginForm;
+use common\models\Product;
 use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
@@ -75,7 +76,11 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $data = Product::find()->all();
+
+        return $this->render('index',[
+            'products' => $data
+        ]);
     }
 
     /**
