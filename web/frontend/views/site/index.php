54ce5e2f3378d3dc70793b852a7ad3b373068aa6
Psousa92 <voltaire@live.com.pt>
2024-11-30 23:44:28 +0000

    front page recent products
    updated code of "recent products" on front page

--- web/frontend/views/site/index.php
+++ web/frontend/views/site/index.php
@@ -360,10 +360,61 @@
     <!-- Offer End -->
 
 
-    <!-- Products Start -->
+    <!-- RECENT Products Start -->
     <div class="container-fluid pt-5 pb-3">
         <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Recent Products</span></h2>
         <div class="row px-xl-5">
+
+            <?php 
+                if (empty($products)) {
+                    echo "<p>No products available.</p>";
+                } else {
+                    
+                    usort($products, function($a, $b) {                 //USORT used to sort the card by date, for the newest. 
+                        return strtotime($b->created_at) - strtotime($a->created_at);
+                    });
+
+                    $count=0;
+                    $maxcard=4;
+
+                foreach($products as $product){
+                    if ($count >= $maxcard) break;      //COUNTER is used to limit the max number of cards. RECENT Default set to 4.
+                    
+            ?>
+            <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                <div class="product-item bg-light mb-4">
+                    <div class="product-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="<?= $product->image_url ?>" alt="">
+                        <div class="product-action">
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <a class="h6 text-decoration-none text-truncate" href=""><?= $product->name ?></a>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h5><?= $product->price ?></h5><!-- REMOVED old price<h6 class="text-muted ml-2"><del> old price</del></h6>-->
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mb-1">
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small>(99)</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+                $count++; }};
+            ?>
+
+
+            <!-- HARDCODED PRODUCTS FROM TEMPLATE -->
+              <!-- 
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
@@ -471,7 +522,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             
         </div>
     </div>
