54e7164d19a16cce60630a80bb6eef378d479961
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-12-09 19:22:41 +0000

    Fixed listing card selection bug

--- web/frontend/views/listing/_form.php
+++ web/frontend/views/listing/_form.php
@@ -13,18 +13,43 @@ use yii\jui\AutoComplete;
 
     <?php $form = ActiveForm::begin(); ?>
 
+    <!-- The code below creates a label with auto complete for the cards so that the user can ----
+    ---- search trough the cards and select it by typing the name. When the user selects a card --
+    ---- it's Id is then passed to an hidden field in the form to be submitted. -->
     <label class="control-label" for="listing-price">Card Name</label>
     <?= \yii\jui\AutoComplete::widget([
-        'options' => ['class' => 'form-control', 'placeholder' => 'Type card name...'],
+        'options' => [
+            'class' => 'form-control',
+            'placeholder' => 'Type card name...',
+            'id' => 'autocomplete-card-name',
+        ],
         'clientOptions' => [
             'source' => \yii\helpers\Url::to(['listing/card-list']),
             'minLength' => 2,
             'select' => new \yii\web\JsExpression('function(event, ui) {
-                $("#listing-card_id").val(ui.item.id);
-            }'),
+            $("#listing-card_id").val(ui.item.id);
+        }'),
         ],
     ]);
-    echo $form->field($model, 'card_id')->hiddenInput()->label(false) ?>
+    ?>
+    <?= $form->field($model, 'card_id')->hiddenInput(['id' => 'listing-card_id'])->label(false) ?>
+
+    <?php
+    //Blur checks whether if the field is blank after loosing focus, then clears the hidden field
+    $js = <<<JS
+    $('#autocomplete-card-name').on('blur', function() {
+        var inputVal = $(this).val();
+        if (!inputVal) {
+            $("#listing-card_id").val('');
+        }
+    });
+    //Input clears the hidden field everytime the card name is manually changed
+    $('#autocomplete-card-name').on('input', function() {
+        $("#listing-card_id").val('');
+    });
+JS;
+    $this->registerJs($js);
+    ?>
 
     <div class="row">
         <div class="col-6">
@@ -46,8 +71,8 @@ use yii\jui\AutoComplete;
     </div>
 
     <div class="form-group">
-        <?= Html::a('Create Card', ['/card/create'], ['class' => 'btn btn-primary mr-3']) ?>
         <?= Html::submitButton('Save', ['class' => 'btn btn-success mr-3']) ?>
+        <?= Html::a('Create new Card', ['/card/create'], ['class' => 'btn btn-primary mr-3']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
