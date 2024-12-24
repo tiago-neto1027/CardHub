df3909a8d501d1f0141d2684a87b47a3b716ace1
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2024-12-24 22:45:23 +0000

    Everything is in json now

--- web/backend/modules/api/controllers/BaseActiveController.php
+++ web/backend/modules/api/controllers/BaseActiveController.php
@@ -7,6 +7,21 @@ use yii\rest\ActiveController;
 use yii\web\Response;
 
 class BaseActiveController extends ActiveController{
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['contentNegotiator'] = [
+            'class' => \yii\filters\ContentNegotiator::class,
+            'formats' => [
+                'application/json' => \yii\web\Response::FORMAT_JSON,
+            ],
+        ];
+
+        return $behaviors;
+    }
+
     public function beforeAction($action)
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
