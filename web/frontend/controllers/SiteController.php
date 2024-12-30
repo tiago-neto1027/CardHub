f11ca8b70c2e9b0628fc3592219bf01c3ca5dd0b
GoncaloLeitao2001 <goncalogaspar2001@outlook.pt>
2024-12-30 19:13:01 +0000

    Updated signup
    Upon registration, user will get the buyer role

--- web/frontend/controllers/SiteController.php
+++ web/frontend/controllers/SiteController.php
@@ -190,7 +190,7 @@ class SiteController extends Controller
     {
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
+            Yii::$app->session->setFlash('success', 'Thank you for registration.');
             return $this->goHome();
         }
 
