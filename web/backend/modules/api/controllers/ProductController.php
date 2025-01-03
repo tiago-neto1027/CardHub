fe9eeb7cf0d33725058f4198e0c9d5ad02f48bd0
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2025-01-03 20:38:45 +0000

    Products and Cards now only fetch active

--- web/backend/modules/api/controllers/ProductController.php
+++ web/backend/modules/api/controllers/ProductController.php
@@ -2,9 +2,26 @@
 
 namespace backend\modules\api\controllers;
 
+use common\models\Product;
+use yii\data\ActiveDataProvider;
 use yii\rest\ActiveController;
 
 class ProductController extends BaseActiveController
 {
     public $modelClass = 'common\models\Product';
+
+    //Filter only active products
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
+    public function prepareDataProvider()
+    {
+        $query = Product::find()->where(['status' => 'active']);
+        return new ActiveDataProvider(['query' => $query]);
+    }
 }
