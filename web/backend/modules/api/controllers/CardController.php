4b690a459892b181d048359b1f69bbb74350f1a9
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2025-01-06 22:37:27 +0000

    Update CardController.php

--- web/backend/modules/api/controllers/CardController.php
+++ web/backend/modules/api/controllers/CardController.php
@@ -6,7 +6,6 @@ use common\models\Card;
 use common\models\Listing;
 use Yii;
 use yii\data\ActiveDataProvider;
-use yii\filters\auth\HttpBasicAuth;
 use yii\rest\ActiveController;
 use yii\web\ForbiddenHttpException;
 use yii\web\Response;
@@ -14,31 +13,6 @@ use yii\web\Response;
 class CardController extends BaseActiveController
 {
     public $modelClass = 'common\models\Card';
-
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-
-        // Enable authentication for all actions by default
-        $behaviors['authenticator'] = [
-            'class' => HttpBasicAuth::class,
-        ];
-
-        return $behaviors;
-    }
-
-    // This method will be used to allow access without authentication for 'countlistings' action
-    public function beforeAction($action)
-    {
-        // Skip authentication for the 'countlistings' action
-        if ($action->id === 'countlistings') {
-            return true;  // Allow access without authentication
-        }
-
-        // Otherwise, proceed with authentication for other actions
-        return parent::beforeAction($action);  // Default authentication behavior
-    }
-
 
     //Filter only active cards
     public function actions()
@@ -57,10 +31,24 @@ class CardController extends BaseActiveController
         return new ActiveDataProvider(['query' => $query]);
     }
 
-    public function actionCountListings($id){
+    public function actionCountlistings($id){
         $count = Listing::find()->where(['card_id' => $id])->count();
         Yii::$app->response->format = Response::FORMAT_JSON;
         return ['listingCount' => $count];
     }
 
+    public function actionCreate()
+    {
+        throw new ForbiddenHttpException('Create action is disabled.');
+    }
+
+    public function actionUpdate($id)
+    {
+        throw new ForbiddenHttpException('Update action is disabled.');
+    }
+
+    public function actionDelete($id)
+    {
+        throw new ForbiddenHttpException('Delete action is disabled.');
+    }
 }
