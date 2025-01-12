395d8cc21e914ac85ca45d4551e6bbe1db8ebc89
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2025-01-12 20:03:10 +0000

    Main App done

--- android/app/src/main/java/com/example/cardhub/AppMainActivity.java
+++ android/app/src/main/java/com/example/cardhub/AppMainActivity.java
@@ -1,34 +1,73 @@
 package com.example.cardhub;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class AppMainActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener {
+public class AppMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public static final String USERNAME = "USERNAME";
+
     private FragmentManager fragmentManager;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+        navigationView.setNavigationItemSelectedListener(this);
 
         fragmentManager = getSupportFragmentManager();
+        loadInitialFragment();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        Fragment fragment = null;
+
+        if(item.getItemId() == R.id.cardsList) {
+            fragment = new CardsFragment();
+            setTitle(item.getTitle());
+        }
+        //TODO: Add new fragments here
+
+        if(fragment != null)
+            fragmentManager.beginTransaction().replace(R.id.contentFragment,fragment).commit();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
+    private boolean loadInitialFragment(){
+        Menu menu = navigationView.getMenu();
+        MenuItem item = menu.getItem(0);
+        item.setCheckable(true);
+        return onNavigationItemSelected(item);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 }
