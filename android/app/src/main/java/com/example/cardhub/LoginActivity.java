2f67d5f44e6dcf87737ecb8d97cb08591b143add
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2025-01-15 21:55:18 +0000

    Settings button working again

--- android/app/src/main/java/com/example/cardhub/LoginActivity.java
+++ android/app/src/main/java/com/example/cardhub/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.cardhub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -36,5 +38,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, AppMainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void navigateToSettings(View view) {
+        Intent intent = new Intent(LoginActivity.this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
