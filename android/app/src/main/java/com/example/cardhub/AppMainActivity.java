495f4187d194eb2ece6726652d1602bbdb03ffd9
GoncaloLeitao2001 <goncalogaspar2001@outlook.pt>
2025-01-16 15:02:46 +0000

    navbar

--- android/app/src/main/java/com/example/cardhub/AppMainActivity.java
+++ android/app/src/main/java/com/example/cardhub/AppMainActivity.java
@@ -1,107 +1,65 @@
 package com.example.cardhub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.example.cardhub.controllers.CardController;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import models.Card;
 
-public class AppMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    public static final String CARD_ID = "CARD_ID";
-
-    private CardController cardController;
-
+public class AppMainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
-    private DrawerLayout drawer;
-    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        drawer = findViewById(R.id.drawerLayout);
-        navigationView = findViewById(R.id.navView);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
-        toggle.syncState();
-        drawer.addDrawerListener(toggle);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        cardController = new CardController(this);
         fragmentManager = getSupportFragmentManager();
 
-        int cardId = getIntent().getIntExtra(CARD_ID, -1);
-        if (cardId != -1) {
-            showListingsFragment(cardId);
-        } else{
-            loadInitialFragment();
-        }
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment fragment = null;
+    /**
+     * Handle BottomNavigationView item selection
+     */
+    private boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent = null;
 
-        if(item.getItemId() == R.id.cardsList) {
-            fragment = new CardsFragment();
+        if (item.getItemId() == R.id.nav_home) {
+            //intent = new Intent(this, SettingsActivity.class);
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.nav_wishlist) {
+            // Launch Wishlist Activity
+            //intent = new Intent(this, WishlistActivity.class);
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.nav_shop) {
+            intent = new Intent(this, ShopActivity.class);
+            setTitle(item.getTitle());
+        } else if (item.getItemId() == R.id.nav_profile) {
+            //intent = new Intent(this, ProfileActivity.class);
             setTitle(item.getTitle());
         }
-        if(item.getItemId() == R.id.productList) {
-            fragment = new ProductsFragment();
-            setTitle(item.getTitle());
+
+        if (intent != null) {
+            startActivity(intent);
+            return true;
         }
-        if(item.getItemId() == R.id.listingsList) {
-            fragment = new ListingsFragment();
-            setTitle(item.getTitle());
-        }
-        //TODO: Add new fragments here
 
-        if(fragment != null)
-            fragmentManager.beginTransaction().replace(R.id.contentFragment,fragment).commit();
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
     }
 
-    private boolean loadInitialFragment(){
-        Menu menu = navigationView.getMenu();
-        MenuItem item = menu.getItem(0);
-        item.setCheckable(true);
-        return onNavigationItemSelected(item);
-    }
 
-    private void showListingsFragment(int cardId) {
-        ListingsFragment listingsFragment = new ListingsFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt(ListingsFragment.CARD_ID, cardId);
-        listingsFragment.setArguments(bundle);
-
-        Card tempCard = cardController.fetchCardDB(cardId);
-        setTitle(tempCard.getName());
-
-        fragmentManager.beginTransaction()
-                .replace(R.id.contentFragment, listingsFragment)
-                .commit();
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
-    }
 }
