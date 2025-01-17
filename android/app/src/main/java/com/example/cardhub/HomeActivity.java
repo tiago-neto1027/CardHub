5d7822a1c9e5e92b02d6357f82390fdd76fe3113
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2025-01-17 08:13:19 +0000

    Themes

--- android/app/src/main/java/com/example/cardhub/HomeActivity.java
+++ android/app/src/main/java/com/example/cardhub/HomeActivity.java
@@ -2,22 +2,14 @@ package com.example.cardhub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import com.example.cardhub.controllers.CardController;
-import com.example.cardhub.controllers.FavoriteController;
-import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
@@ -60,10 +52,5 @@ public class HomeActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
-
-    public void navigateToSettings(View view) {
-        Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
-        startActivity(intent);
     }
 }
